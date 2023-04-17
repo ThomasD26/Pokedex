ea14068b9a991ea58a7000b18bd33a73fbcd8a10
ThomasD26 <thomas@ThomasD>
2023-04-17 12:02:22 +0200

    React 8

--- my-new-app/src/components/Navbar.jsx
+++ my-new-app/src/components/Navbar.jsx
@@ -1,16 +1,13 @@
 import { useState } from 'react'
 
 
-function Navbar ({PokemonIndex, setPokemonIndex, pokemonList}){
-  const handleClickPrécedent = () => setPokemonIndex (old => old -1)
-  const handleClickSuivant = () => setPokemonIndex (old => old +1)
-
-    return(
-    <>
-        {PokemonIndex > 0 && <button onClick={handleClickPrécedent}>Précedent</button>}
-        {PokemonIndex < (pokemonList.length -1) && <button onClick={handleClickSuivant}>Suivant</button>}  
-    </>
-      )
+function Navbar ({setPokemonIndex, pokemonList}){
+  const handleClickPokemon = index => setPokemonIndex (index)
+ 
+   return pokemonList.map(
+    (pokemon, index) => <button onClick={() => handleClickPokemon(index)}>{pokemon.name}</button>
+    )
+      
 }
 
 
